f251d782b0e220ef8b1d62293c4e59cd79da68e6
Shane Duan <shane.duan@workday.com>
2022-12-13 17:39:47 -0800

    day 10 solution

---
+++ src/io/duan/advent/y2022/Day10.java
@@ -0,0 +1,71 @@
+package io.duan.advent.y2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Day10 {
+    public static void main(String... args) throws IOException {
+        var cpu = new Cpu();
+        var signal = new Signal();
+        Files.lines(Path.of("day10.txt")).forEach(line -> {
+            var from = cpu.getCurrentCycle();
+            var x = cpu.process(line);
+            System.out.println(from + ":" + line + " -> " + x);
+            signal.check(from, cpu.getCurrentCycle(), x);
+        });
+        System.out.println("Total " + signal.getTotal());
+    }
+
+    static class Cpu {
+        private int cycle = 1;
+        private int register = 1;
+
+        public int process(String instruction) {
+            var value = this.register;
+            var elements = instruction.split(" ");
+            switch (elements[0]) {
+                case "addx" -> {
+                    this.register += Integer.parseInt(elements[1]);
+                    cycle += 2;
+                }
+                case "noop" -> cycle += 1;
+                default -> throw new IllegalArgumentException("cannot process " + instruction);
+            }
+            return value;
+        }
+
+        public int getCurrentCycle() {
+            return this.cycle;
+        }
+
+    }
+
+    static class Signal {
+        private final int[] cycles = {20, 60, 100, 140, 180, 220};
+        private int sum = 0;
+        public void check(int from, int toExclusive, int register) {
+            var cycle = checkRange(from, toExclusive);
+            if (cycle != -1) {
+                sum += cycle * register;
+            }
+        }
+
+        private int checkRange(int from, int toExclusive) {
+            for (int cycle : cycles) {
+                if (from <= cycle) {
+                    if (toExclusive > cycle) {
+                        return cycle;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public int getTotal() {
+            return this.sum;
+        }
+    }
+}
---
+++ src/io/duan/advent/y2022/Day10Part2.java
@@ -0,0 +1,51 @@
+package io.duan.advent.y2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.IntStream;
+
+public class Day10Part2 {
+    public static void main(String... args) throws IOException {
+        var sprite = new Sprite();
+        Files.lines(Path.of("day10.txt")).forEach(line -> {
+            sprite.drawAndExecute(line);
+        });
+        sprite.print();
+    }
+
+    static class Sprite {
+        private int cycle = 0;
+        private int sprite = 1;
+        private StringBuilder buffer = new StringBuilder();
+        public void drawAndExecute(String line) {
+            var elements = line.split(" ");
+            switch (elements[0]) {
+                case "addx" -> {
+                    draw();
+                    draw();
+                    this.sprite += Integer.parseInt(elements[1]);
+                }
+                case "noop" -> draw();
+                default -> throw new IllegalArgumentException("cannot process " + line);
+            }
+        }
+
+        private void draw() {
+            var index = cycle % 40;
+            if (index >= sprite - 1 && index <= sprite + 1) {
+                buffer.append("#");
+            } else {
+                buffer.append(".");
+            }
+            cycle++;
+        }
+
+        public void print() {
+            IntStream.range(0, 6).forEach(row -> {
+                var start = row * 40;
+                System.out.println(buffer.substring(start, start+40));
+            });
+        }
+    }
+}
